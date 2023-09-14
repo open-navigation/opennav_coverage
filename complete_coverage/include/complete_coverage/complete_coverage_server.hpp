e7a602e83e6d05f07cbe669ca22cbebbd1da9ae5
stevemacenski <stevenmacenski@gmail.com>
2023-09-14 14:55:13 -0700

    adding basic lifecycle-component node structure for server + adding msg interface package template

---
+++ complete_coverage/include/complete_coverage/complete_coverage_server.hpp
@@ -0,0 +1,92 @@
+// Copyright (c) 2023 Open Navigation LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef COMPLETE_COVERAGE__COMPLETE_COVERAGE_SERVER_HPP_
+#define COMPLETE_COVERAGE__COMPLETE_COVERAGE_SERVER_HPP_
+
+#include "rclcpp/rclcpp.hpp"
+#include "nav2_util/lifecycle_node.hpp"
+
+namespace complete_coverage
+{
+/**
+ * @class complete_coverage::CompleteCoverageServer
+ * @brief An action server which implements highly reconfigurable complete
+ * coverage planning using the Fields2Cover library
+ */
+class CompleteCoverageServer : public nav2_util::LifecycleNode
+{
+public:
+  /**
+   * @brief A constructor for complete_coverage::CompleteCoverageServer
+   * @param options Additional options to control creation of the node.
+   */
+  explicit CompleteCoverageServer(const rclcpp::NodeOptions & options = rclcpp::NodeOptions());
+
+  /**
+   * @brief A destructor for complete_coverage::CompleteCoverageServer
+   */
+  ~CompleteCoverageServer() = default;
+
+protected:
+  /**
+   * @brief Configure member variables
+   * @param state Reference to LifeCycle node state
+   * @return SUCCESS or FAILURE
+   */
+  nav2_util::CallbackReturn on_configure(const rclcpp_lifecycle::State & state) override;
+
+  /**
+   * @brief Activate member variables
+   * @param state Reference to LifeCycle node state
+   * @return SUCCESS or FAILURE
+   */
+  nav2_util::CallbackReturn on_activate(const rclcpp_lifecycle::State & state) override;
+
+  /**
+   * @brief Deactivate member variables
+   * @param state Reference to LifeCycle node state
+   * @return SUCCESS or FAILURE
+   */
+  nav2_util::CallbackReturn on_deactivate(const rclcpp_lifecycle::State & state) override;
+
+  /**
+   * @brief Reset member variables
+   * @param state Reference to LifeCycle node state
+   * @return SUCCESS or FAILURE
+   */
+  nav2_util::CallbackReturn on_cleanup(const rclcpp_lifecycle::State & state) override;
+
+  /**
+   * @brief Called when in shutdown state
+   * @param state Reference to LifeCycle node state
+   * @return SUCCESS or FAILURE
+   */
+  nav2_util::CallbackReturn on_shutdown(const rclcpp_lifecycle::State & state) override;
+
+  /**
+   * @brief Callback executed when a parameter change is detected
+   * @param event ParameterEvent message
+   */
+  rcl_interfaces::msg::SetParametersResult
+  dynamicParametersCallback(std::vector<rclcpp::Parameter> parameters);
+
+  // Dynamic parameters handler
+  rclcpp::node_interfaces::OnSetParametersCallbackHandle::SharedPtr dyn_params_handler_;
+  std::mutex dynamic_params_lock_;
+};
+
+}  // namespace complete_coverage
+
+#endif  // COMPLETE_COVERAGE__COMPLETE_COVERAGE_SERVER_HPP_
---
+++ complete_coverage/src/complete_coverage_server.cpp
@@ -0,0 +1,106 @@
+// Copyright (c) 2023 Open Navigation LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "complete_coverage/complete_coverage_server.hpp"
+
+using namespace std::chrono_literals;
+using rcl_interfaces::msg::ParameterType;
+using std::placeholders::_1;
+
+namespace complete_coverage
+{
+
+CompleteCoverageServer::CompleteCoverageServer(const rclcpp::NodeOptions & options)
+: nav2_util::LifecycleNode("complete_coverage_server", "", options)
+{
+  RCLCPP_INFO(get_logger(), "Creating %s", get_name());
+
+}
+
+nav2_util::CallbackReturn
+CompleteCoverageServer::on_configure(const rclcpp_lifecycle::State & /*state*/)
+{
+  RCLCPP_INFO(get_logger(), "Configuring %s", get_name());
+
+  return nav2_util::CallbackReturn::SUCCESS;
+}
+
+nav2_util::CallbackReturn
+CompleteCoverageServer::on_activate(const rclcpp_lifecycle::State & /*state*/)
+{
+  RCLCPP_INFO(get_logger(), "Activating %s", get_name());
+  auto node = shared_from_this();
+
+  // Add callback for dynamic parameters
+  dyn_params_handler_ = node->add_on_set_parameters_callback(
+    std::bind(&CompleteCoverageServer::dynamicParametersCallback, this, _1));
+
+  // create bond connection
+  createBond();
+
+  return nav2_util::CallbackReturn::SUCCESS;
+}
+
+nav2_util::CallbackReturn
+CompleteCoverageServer::on_deactivate(const rclcpp_lifecycle::State & /*state*/)
+{
+  RCLCPP_INFO(get_logger(), "Deactivating %s", get_name());
+
+  dyn_params_handler_.reset();
+
+  // destroy bond connection
+  destroyBond();
+
+  return nav2_util::CallbackReturn::SUCCESS;
+}
+
+nav2_util::CallbackReturn
+CompleteCoverageServer::on_cleanup(const rclcpp_lifecycle::State & /*state*/)
+{
+  RCLCPP_INFO(get_logger(), "Cleaning up %s", get_name());
+
+  return nav2_util::CallbackReturn::SUCCESS;
+}
+
+nav2_util::CallbackReturn
+CompleteCoverageServer::on_shutdown(const rclcpp_lifecycle::State &)
+{
+  RCLCPP_INFO(get_logger(), "Shutting down %s", get_name());
+  return nav2_util::CallbackReturn::SUCCESS;
+}
+
+rcl_interfaces::msg::SetParametersResult
+CompleteCoverageServer::dynamicParametersCallback(std::vector<rclcpp::Parameter> parameters)
+{
+  std::lock_guard<std::mutex> lock(dynamic_params_lock_);
+  rcl_interfaces::msg::SetParametersResult result;
+  for (auto parameter : parameters) {
+    // const auto & type = parameter.get_type();
+    // const auto & name = parameter.get_name();
+
+    //TODO
+  }
+
+  result.successful = true;
+  return result;
+}
+
+}  // namespace complete_coverage
+
+#include "rclcpp_components/register_node_macro.hpp"
+
+// Register the component with class_loader.
+// This acts as a sort of entry point, allowing the component to be discoverable when its library
+// is being loaded into a running process.
+RCLCPP_COMPONENTS_REGISTER_NODE(complete_coverage::CompleteCoverageServer)
