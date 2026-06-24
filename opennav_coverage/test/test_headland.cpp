f53ef1e8e94d3dc18cbe1f7d6b01b04414d353bf
u33549 <cebburumutcan33549@gmail.com>
2026-06-24 14:14:49 +0300

    A.13: migrate tests to v2 API (Path::getStates, getField, getMaxDiffCurv)

--- opennav_coverage/test/test_headland.cpp
+++ opennav_coverage/test/test_headland.cpp
@@ -80,13 +80,13 @@ TEST(HeadlandTests, TestheadlandGeneration)
 
   // Generate some toy field
   f2c::Random rand;
-  auto field = rand.generateRandField(5, 1e5);
+  auto field = rand.generateRandField(1e5, 5);
 
   // Shouldn't throw, results in valid output
   opennav_coverage_msgs::msg::HeadlandMode settings;
-  auto field_out = generator.generateHeadlands(field.field.getGeometry(0), settings);
+  auto field_out = generator.generateHeadlands(field.getField().getGeometry(0), settings);
   settings.mode = "CONSTANT";
-  auto field_out2 = generator.generateHeadlands(field.field.getGeometry(0), settings);
+  auto field_out2 = generator.generateHeadlands(field.getField().getGeometry(0), settings);
 }
 
 }  // namespace opennav_coverage
